ffd0fc9aa2eac18aa1abde25d810f67f96ffae55
Erik Russell <erikrussells@gmail.com>
2022-11-04 15:48:54 -0400

    commit tree 667b6118ed57e9132a42a86aa3a7ac655db821a0

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-function TaskList() {
+function TaskList({ tasks }) {
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasks.map((task) => {
+        return (
+          <task 
+          text={task.text}
+          category={task.category}
+          />
+        )
+      })}
     </div>
   );
 }
